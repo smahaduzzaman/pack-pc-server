188dd80113f2f0d4999006bb8f7077a3a4e74785
smahaduzzaman <smahaduzzaman96@gmail.com>
2023-07-30 19:31:48 +0600

    Create .env File

--- index.js
+++ index.js
@@ -8,32 +8,56 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-const username = "packpc";
-const password = "SAim654oTswoQaIk";
+const username = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
 
-const uri = `mongodb+srv://${username}:${password}@cluster0.fceds.mongodb.net/?retryWrites=true&w=majority`
-console.log(uri)
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const uri = `mongodb+srv://${username}:${password}@cluster0.fceds.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-    try {
-        await client.connect();
-        console.log('Database Connected Successfully')
-        const productsCollection = client.db('packpc').collection('products');
-        // const categoriesCollection = client.db('packpc').collection('categories');
+  try {
+    await client.connect();
+    console.log("Database Connected Successfully");
+    const productsCollection = client.db("packpc").collection("products");
+    const categoriesCollection = client.db("packpc").collection("categories");
 
-        app.get('/products', async (req, res) => {
-            const cursor = productsCollection.find({});
-            const products = await cursor.toArray();
-            res.send(products);
-        })
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find({});
+      const products = await cursor.toArray();
+      res.send(products);
+    });
 
-        
-    } catch (error) {
-        console.log(error.stack)
-    } finally {
-        // await client.close();
-    }
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const product = await productsCollection.findOne(query);
+      res.send(product);
+    });
+
+    app.get("/categories", async (req, res) => {
+      const cursor = categoriesCollection.find({});
+      // cursor.sort({ name: 1 });
+      const categories = await cursor.toArray();
+      res.send(categories);
+    });
+
+    app.get("/products/category/:category", async (req, res) => {
+      const category = req.params.category;
+      const query = { category: category };
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } catch (error) {
+    console.log(error.stack);
+  } finally {
+    // await client.close();
+  }
 }
 run().catch(console.dir);
 
